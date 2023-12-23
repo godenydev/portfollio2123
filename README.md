34908b5a914b7196be34c24f7ccd54d7edf8f9bb
Godeny <152424493+godenydev@users.noreply.github.com>
2023-12-23 18:51:35 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# portfollio2123
-ozel
+# mustifixwashere.vercel.app
+
+This open source project is my active website built using React, Vite and Tailwindcss.
+
+> Designer and coding: @mustifix
---
+++ src/config.ts
@@ -0,0 +1,39 @@
+import { BsDiscord, BsInstagram, BsSteam, BsGithub, Bs123, BsYoutube, BsTwitch } from 'react-icons/bs';
+import { IoLogoJavascript, IoLogoCss3 } from 'react-icons/io';
+import { AiFillHtml5 } from 'react-icons/ai'
+import { SiTypescript, SiVite } from 'react-icons/si';
+import { DiNodejsSmall, DiReact, DiVisualstudio } from 'react-icons/di';
+import { TbBrandNextjs } from 'react-icons/tb';
+
+export default {
+    'github': {
+        'username': 'Godeny'
+    },
+    'discord': {
+        'id': '1159843729771868181'
+    },
+    'social_media_accounts': [
+        {
+            'label': 'Discord Users',
+            'to': 'https://discord.com/users/1159843729771868181',
+            'icon': BsDiscord,
+        },
+        {
+            'label': 'Discord Server',
+            'to': 'https://discord.gg/NetmysN9VA',
+            'icon': BsDiscord,
+        },
+
+        ],
+    'techs': [
+        {
+            'lang': 'Twitch',
+            'icon': BsTwitch
+        },
+        {
+            'lang': 'YouTube',
+            'icon': BsYoutube
+        },
+    ]
+}
+
